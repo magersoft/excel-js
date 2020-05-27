ac19fd64f3e015ae0dd109af5a7e247f8b6dc3af
v.mager <v.mager@b2b-center.ru>
2020-05-27 17:51:26 +0300

    finish base framework functional

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { Header } from './header/Header';
+export { Toolbar } from './toolbar/Toolbar';
+export { Formula } from './formula/Formula';
+export { Table } from './table/Table';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './scss/index.scss';
+import { Excel } from './components/excel/Excel';
+import { Formula, Header, Table, Toolbar } from './components';
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+});
+
+excel.render();
