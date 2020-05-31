7319998a4c69326d7bf3aa94e6e9b51aa4c3e4ac
v.mager <v.mager@b2b-center.ru>
2020-05-31 22:35:39 +0300

    Done table keys event and selecting cells

---
+++ README.md
@@ -0,0 +1,8 @@
+# Excel JS
+
+Pure JavaScript
+
+TODO
+* [x] Arrow key navigation
+* [ ] TypeScript 😍
+* [ ] Database and backend 
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,24 +1,76 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from '@/components/table/table.template';
 import { resizeHandler } from '@/components/table/table.resize';
-import { shouldResize } from '@/components/table/table.helpers';
+import {
+  isCell,
+  shouldResize,
+  matrix,
+  nextSelector
+} from '@/components/table/table.helpers';
+import { TableSelection } from '@/components/table/TableSelection';
+import { $ } from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
+  static rowCounts = 20;
 
   constructor($root) {
     super($root, {
-      listeners: ['mousedown']
+      listeners: ['mousedown', 'keydown']
     });
+  }
+
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selection.select($cell);
   }
 
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+            .map(id => this.$root.find(`[data-id="${id}"]`));
+
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
+    }
+  }
+
+  onKeydown(event) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowUp',
+      'ArrowDown'
+    ];
+
+    const { key, shiftKey } = event;
+
+    if (keys.includes(key)) {
+      if (key !== 'Enter') {
+        event.preventDefault();
+      }
+      const id = this.selection.current.id(true);
+      const $next = this.$root
+          .find(nextSelector(key, shiftKey, id, event, Table.rowCounts));
+      this.selection.select($next);
     }
   }
 
   toHTML() {
-    return createTable(20);
+    return createTable(Table.rowCounts);
   }
 }
