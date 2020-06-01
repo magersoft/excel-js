cd24dfd0d876f3e9b1d78a48725f348a1d5ea7ee
v.mager <v.mager@b2b-center.ru>
2020-06-01 15:44:41 +0300

    createStore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import './scss/index.scss';
 import { Excel } from './components/excel/Excel';
 import { Formula, Header, Table, Toolbar } from './components';
+import { createStore } from '@core/redux/createStore';
+import { rootReducer } from '@/store/rootReducer';
+
+import './scss/index.scss';
+
+const store = createStore(rootReducer);
 
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 });
 
 excel.render();
