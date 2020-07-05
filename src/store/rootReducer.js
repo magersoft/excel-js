7e9b5a5217b61a40f59961b8a5b60e5dd425e0c2
v.mager <v.mager@b2b-center.ru>
2020-07-05 22:30:16 +0300

    routing

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,7 +3,8 @@ import {
   CHANGE_STYLES,
   TABLE_RESIZE,
   APPLY_STYLE,
-  CHANGE_TITLE
+  CHANGE_TITLE,
+  UPDATE_DATE
 } from '@/store/types';
 
 export function rootReducer(state, action) {
@@ -48,6 +49,9 @@ export function rootReducer(state, action) {
 
     case CHANGE_TITLE:
       return { ...state, title: action.data };
+
+    case UPDATE_DATE:
+      return { ...state, openedDate: new Date().toJSON() };
 
     default: return state;
   }
